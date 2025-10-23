cd79c8366d48251152cb5f8d18843799197aca87
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-23 15:58:01 +0700

    Tambah composable ActivitasPertama

--- app/src/main/java/com/example/praktikum4/UkPertama.kt
+++ app/src/main/java/com/example/praktikum4/UkPertama.kt
@@ -24,3 +24,12 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+@Composable
+fun ActivitasPertama(modifier: Modifier) {
+    Column(
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
