16a528e21dd60d6835a95112fbde4a786f8b1cfc
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-23 16:09:32 +0700

    Tambah teks copyright

--- app/src/main/java/com/example/praktikum4/UkPertama.kt
+++ app/src/main/java/com/example/praktikum4/UkPertama.kt
@@ -81,4 +81,13 @@ fun ActivitasPertama(modifier: Modifier) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-        )
+        ) {
+            Text(
+                stringResource(R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
+    }
+}
