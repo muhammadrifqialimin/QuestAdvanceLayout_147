377f2b9849f7b786fc16c195c784ccd58999a1eb
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-23 16:02:51 +0700

    Tambah gambar pramuka

--- app/src/main/java/com/example/praktikum4/UkPertama.kt
+++ app/src/main/java/com/example/praktikum4/UkPertama.kt
@@ -50,4 +50,13 @@ fun ActivitasPertama(modifier: Modifier) {
             colors = CardDefaults.cardColors(
                 containerColor = Color.Cyan
             )
-        )
+        ) {
+            Row() {
+                val gambar = painterResource(R.drawable.pramuka)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(5.dp)
+                )
