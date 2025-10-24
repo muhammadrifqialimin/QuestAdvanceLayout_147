b0eb744b53fe5317d2e07eea646b5869c428dc16
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-24 20:23:21 +0700

    pisahkan Spacer agar struktur Row lebih rapi

--- app/src/main/java/com/example/praktikum4/UkPertama.kt
+++ app/src/main/java/com/example/praktikum4/UkPertama.kt
@@ -95,6 +95,7 @@ fun ActivitasPertama(modifier: Modifier) {
                         .size(100.dp)
                         .padding(5.dp)
                 )
+                Spacer(modifier = Modifier.width(30.dp))
         Box(
             modifier = Modifier
                 .fillMaxSize()
